44a20617f3ffa1970eaaad567842483b775a1d3f
Tarunpreet Ubhi <ubhits@outlook.com>
2022-09-28 16:56:13 -0700

    fixed terminate script

--- base/terminate.ps1
+++ base/terminate.ps1
@@ -4,21 +4,21 @@
 
 # -f Microsoft.Compute/virtualMachineScaleSets
 
-$primarySecuredAADDSConfig = Get-Content ..\deployments\aadds\config\primary.json -Raw | ConvertFrom-Json
+$primarySecuredAADDSConfig = Get-Content .\config\aadds.json -Raw | ConvertFrom-Json
 
 az group delete --no-wait -f Microsoft.Compute/virtualMachines -n $primarySecuredAADDSConfig.rg.name -y
 
-$primarySecuredFWConfig = Get-Content ..\deployments\hub-firewall-spokes-vms\config\primary.json -Raw | ConvertFrom-Json
-$secondarySecuredFWConfig = Get-Content ..\deployments\hub-firewall-spokes-vms\config\secondary.json -Raw | ConvertFrom-Json
-$tertiarySecuredFWConfig = Get-Content ..\deployments\hub-firewall-spokes-vms\config\tertiary.json -Raw | ConvertFrom-Json
+$primarySecuredFWConfig = Get-Content ..\deployments\04-hub-firewall-spokes-vms\config\primary.json -Raw | ConvertFrom-Json
+$secondarySecuredFWConfig = Get-Content ..\deployments\04-hub-firewall-spokes-vms\config\secondary.json -Raw | ConvertFrom-Json
+$tertiarySecuredFWConfig = Get-Content ..\deployments\04-hub-firewall-spokes-vms\config\tertiary.json -Raw | ConvertFrom-Json
 
 az group delete --no-wait -f Microsoft.Compute/virtualMachines -n $primarySecuredFWConfig.rg.name -y
 az group delete --no-wait -f Microsoft.Compute/virtualMachines -n $secondarySecuredFWConfig.rg.name -y
 az group delete --no-wait -f Microsoft.Compute/virtualMachines -n $tertiarySecuredFWConfig.rg.name -y
 
-$primaryBaseConfig = Get-Content .\config\primary.json -Raw | ConvertFrom-Json
-$secondaryBaseConfig = Get-Content .\config\secondary.json -Raw | ConvertFrom-Json
-$tertiaryBaseConfig = Get-Content .\config\tertiary.json -Raw | ConvertFrom-Json
+$primaryBaseConfig = Get-Content .\config\base-primary.json -Raw | ConvertFrom-Json
+$secondaryBaseConfig = Get-Content .\config\base-secondary.json -Raw | ConvertFrom-Json
+$tertiaryBaseConfig = Get-Content .\config\base-tertiary.json -Raw | ConvertFrom-Json
 
 az group delete --no-wait -f Microsoft.Compute/virtualMachines -n $primaryBaseConfig.rg.name -y
 az group delete --no-wait -f Microsoft.Compute/virtualMachines -n $secondaryBaseConfig.rg.name -y
